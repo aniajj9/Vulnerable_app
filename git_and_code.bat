ae95f32efe6a4eeafaa3ba3b77f112f70e683c3d
Anna Gorska <ago@bankdata.dk>
2023-09-17 12:54:00 +0200

    gitignore3

--- git_and_code.bat
+++ git_and_code.bat
@@ -1,1 +1,12 @@
-git add * && git commit -m "%~1" && git push && "c:\Users\bduago\OneDrive - Bankdata\Skrivebord\git\Vuln-app\.venv\Scripts\python.exe" "c:\Users\bduago\OneDrive - Bankdata\Skrivebord\git\Vuln-app\main.py"
+@echo off
+
+rem Check if there are changes to commit
+git status | findstr /i "Changes not staged for commit" > nul
+if errorlevel 1 (
+    echo No changes to commit.
+) else (
+    git add * && git commit -m "%~1" && git push
+)
+
+rem Run the Flask app regardless of changes
+"c:\Users\bduago\OneDrive - Bankdata\Skrivebord\git\Vuln-app\.venv\Scripts\python.exe" "c:\Users\bduago\OneDrive - Bankdata\Skrivebord\git\Vuln-app\main.py"
