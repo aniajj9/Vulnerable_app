0dbba29bd08a12acb52da5d98917520b9c37aa3a
Anna Gorska <ago@bankdata.dk>
2023-09-17 16:03:11 +0200

    database migration

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, session, abort
+from flask_sqlalchemy import SQLAlchemy
 from utils.password_hashing import PasswordHash
 import json
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
-user_data = {} # Initialize an empty dictionary to store user data
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-def load_user_data():
-    try:
-        with open('database.json', 'r') as db_file:
-            return json.load(db_file)
-    except FileNotFoundError:
-        return {}
+# database model
+class Users(db.Model):
+    id = db.Column(db.Integer)
+    username = db.Column(db.String(50), primary_key=True)
+    passwordHash = db.Column(db.String(200), nullable=False)
+    cpr = db.Column(db.String(9), nullable=False)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,40 +24,41 @@ def login():
         username = request.form['username']
         password = request.form['password']
         
-        # Load user data from the JSON file
-        user_data = load_user_data()
+        # Query the database to check if the username exists
+        user = Users.query.filter_by(username=username).first()
         
         # Check if the username exists in the loaded user data
-        if username in user_data and password_hashing.verify_password(user_data[username]["password_hash"], password):
+        if user and password_hashing.verify_password(user.passwordHash, password):
             session['username'] = username
-            # Retrieve the CPR for the logged-in user
-            cpr = user_data[username]["cpr"]
-            return render_template('home.html', cpr=cpr)
+            cpr = user.cpr
+            return redirect(url_for('home', submenu=None))  # Redirect to the "home" route
+    
         else:
             error_message = "Invalid username or password. Please try again."
     
     return render_template('login.html', error_message=error_message)
-
-
-    
-    
+ 
+  
 @app.route('/home', methods=['GET', 'POST'])
 @app.route('/home/<submenu>', methods=['GET', 'POST'])
 def home(submenu=None):
-    user_data = load_user_data()
-    if submenu is None:
-        return render_template("home.html", submenu=submenu)
-    for username, user_info in user_data.items():
-        if 'cpr' in user_info and submenu == user_info['cpr']:
-            return render_template("home.html", submenu=submenu)
-    else:
-        # Return a "Not Found" response
-        abort(404)
+    username = session.get('username', None)
+    
+    if username:
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            if submenu is None:
+                return render_template("home.html", submenu=submenu, cpr=user.cpr)
+            else:
+                # Check if 'submenu' exists in the 'cpr' field of ANY user
+                matching_users = Users.query.filter(Users.cpr == submenu).all()
+                if matching_users:
+                    return render_template("home.html", submenu=submenu, cpr=user.cpr)
+    
+    # If no valid user or matching CPR is found, return a "Not Found" response
+    abort(404)
 
-        username = session['username']
-        
-        # Retrieve the CPR of the currently logged-in user
-        current_user_cpr = user_data.get(username, {}).get('cpr', None)
+
 
 @app.route('/logout')
 def logout():
@@ -70,34 +74,34 @@ def register():
         password = request.form['password']
         cpr = request.form['cpr']
         
-        # Load user data from the JSON file
-        user_data = load_user_data()
+        # Check if the username already exists in the database
+        existing_user = Users.query.filter_by(username=username).first()
         
-        # Check if the username already exists
-        if username in user_data:
+        if existing_user:
             error_message = "Username already exists. Please choose another username."
         else:
-            # Add the new user to the user_data dictionary
+            # Create a new user record and add it to the database
             password_hash = password_hashing.hash_password(password)
-            user_data[username] = {"password_hash": password_hash, "cpr": cpr}
-            
-            # Save the user_data dictionary to the database.json file
-            with open('database.json', 'w') as db_file:
-                json.dump(user_data, db_file)
+            new_user = Users(username=username, passwordHash=password_hash, cpr=cpr)
+            db.session.add(new_user)
+            db.session.commit()
             
             return redirect(url_for('login'))
     
     return render_template('register.html', error_message=error_message)
 
-
+@app.route('/view_users', methods=['GET'])
+def view_users():
+    # Query all user entries from the Users table
+    users = Users.query.all()
+    
+    return render_template('view_users.html', users=users)
 
 if __name__ == "__main__":
-    # Load existing user data from the database.json file if it exists
-    try:
-        with open('database.json', 'r') as db_file:
-            user_data = json.load(db_file)
-    except FileNotFoundError:
-        pass
+
+    with app.app_context():
+        db.create_all()
+
 
     # Initialize password hashing
     hashing_algorithm = "sha256"
