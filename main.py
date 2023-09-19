2950014c8384065e06958b9f571f882037d15d7b
Anna Gorska <ago@bankdata.dk>
2023-09-19 09:27:27 +0200

    formatting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, abort
 from flask_sqlalchemy import SQLAlchemy
 from utils.password_hashing import PasswordHash
-import json
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
